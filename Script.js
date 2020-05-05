f46631d3f696426a1b4a194d56302ef20bc50d2b
Eduardo Job <eduardoJob@users.noreply.github.com>
2020-05-04 21:58:24 -0300

    Seamless button added

--- Script.js
+++ Script.js
@@ -3,9 +3,6 @@ var redirecao=0
 function redirecionar(){
     redirecao=redirecao+1;
     if(redirecao>=5){
-        window.location.replace("index2.html")
+        window.location.replace("02051993.html")
     }
-
 }
-
-alert("welcome to hell")
